29db924371e10454829a459b2025a2313bc8c20b
Rock Zhang <codingbelief@gmail.com>
2016-12-21 20:38:09 +0800

    Updates zh/js/custom.js
    Auto commit by GitBook Editor

--- zh/js/custom.js
+++ zh/js/custom.js
@@ -1,1 +1,7 @@
-/* custom js */
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?4ad50191d5d2bcbc1008997dd0c05245";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
